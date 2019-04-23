a3526c11d234df0334036b86f95b6abb382b5e10
ChrisLaneAU <chrislane10001@gmail.com>
2019-04-24 09:59:04 +1000

    removed duplicate constructor - driver join table

--- db/migrate/20190420121345_create_join_table_constructor_driver.rb
+++
@@ -1,8 +0,0 @@
-class CreateJoinTableConstructorDriver < ActiveRecord::Migration[5.2]
-  def change
-    create_join_table :constructors, :drivers do |t|
-      # t.index [:constructor_id, :driver_id]
-      # t.index [:driver_id, :constructor_id]
-    end
-  end
-end
