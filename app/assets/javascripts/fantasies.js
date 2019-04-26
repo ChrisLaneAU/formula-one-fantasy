af582bf83ca0e814c9219a1bb4e0a9126701b19e
ChrisLaneAU <chrislane10001@gmail.com>
2019-04-26 13:18:15 +1000

    css complete, presentation ready

---
+++ app/assets/javascripts/fantasies.js
@@ -0,0 +1,10 @@
+$(document).ready(() => {
+  // create team
+  // $(".create-fantasy-team-label-check").on("click", e => {
+  //   $(e.target).toggleClass("label-active");
+  // });
+  //
+  // $(".create-fantasy-team-label-radio").on("click", e => {
+  //   $(e.target).toggleClass("label-active");
+  // });
+});
--- app/assets/javascripts/race.js
+++ app/assets/javascripts/race.js
@@ -2,7 +2,7 @@ $(document).ready(() => {
   $(".team-select-container-label").on("click", e => {
     $(e.target).toggleClass("label-active");
   });
-  let seconds = 5;
+  let seconds = 60;
   const timer = setInterval(() => {
     $(".timer").text(` Start: ${seconds}`);
     seconds--;
@@ -11,4 +11,18 @@ $(document).ready(() => {
       $(".timer").text("GO!");
     }
   }, 1000);
+
+  $(".create-fantasy-team-label-check").on("click", e => {
+    $(e.target).toggleClass("label-active");
+  });
+
+  $(".create-fantasy-team-label-radio").on("click", e => {
+    const radioLabels = document.querySelectorAll(
+      ".create-fantasy-team-label-radio"
+    );
+    radioLabels.forEach(label => {
+      //label.classList.remove("label-active");
+    });
+    $(e.target).toggleClass("label-active");
+  });
 });
